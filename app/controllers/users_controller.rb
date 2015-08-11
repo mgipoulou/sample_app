8ab055ab4c81aea3856803fa625543e6ae0ff1fa
Mathieu Gipoulou <mgipoulou@gmail.com>
2015-08-11 22:31:20 +0000

    finish cpt 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   
+   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+   before_action :correct_user,   only: [:edit, :update, :show]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
   def new
     @user = User.new
   end
@@ -18,6 +31,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -25,4 +52,18 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
   end
+      
+  def logged_in_user
+    unless logged_in?
+    store_location
+    flash[:danger] = "Please log in."
+    redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless (@user == current_user)
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Mathieu Gipoulou",
+             email: "mgipoulou@gmail.com",
+             password:              "2613sp47",
+             password_confirmation: "2613sp47",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
