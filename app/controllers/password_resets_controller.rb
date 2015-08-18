0d9b057df9e2f1e4e4a49fa696c6594fda984781
Mathieu Gipoulou <mgipoulou@gmail.com>
2015-08-18 22:25:27 +0000

    chpt10v3

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,9 +1,8 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user,   only: [:edit, :update]
-  before_action :valid_user, only: [:edit, :update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
   before_action :check_expiration, only: [:edit, :update]
 
-  
   def new
   end
 
@@ -22,7 +21,7 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if params[:user][:password].empty?
       flash.now[:danger] = "Password can't be empty"
@@ -35,12 +34,14 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
 
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
+
+    # Before filters
 
     def get_user
       @user = User.find_by(email: params[:email])
@@ -53,12 +54,12 @@ class PasswordResetsController < ApplicationController
         redirect_to root_url
       end
     end
-    
+
+    # Checks expiration of reset token.
     def check_expiration
       if @user.password_reset_expired?
         flash[:danger] = "Password reset has expired."
         redirect_to new_password_reset_url
       end
     end
-  
 end
